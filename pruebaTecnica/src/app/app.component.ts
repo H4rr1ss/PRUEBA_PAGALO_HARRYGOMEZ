cc7bf279f89949fd7e9277259dbf2a531d946b73
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2025-03-25 01:16:10 -0600

    feat: router-outlet

--- pruebaTecnica/src/app/app.component.ts
+++ pruebaTecnica/src/app/app.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styles: [`
+    .router-outlet-container {
+      box-sizing: border-box;
+      font-family: 'Open Sans', sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+  `]
 })
 export class AppComponent {
   title = 'pruebaTecnica';
