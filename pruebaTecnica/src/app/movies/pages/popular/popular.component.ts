59338ef0ddb55e7e10af06d9b197e2e8e48392b5
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2025-03-25 01:57:27 -0600

    feat(movies): popular page

--- pruebaTecnica/src/app/movies/pages/popular/popular.component.ts
+++ pruebaTecnica/src/app/movies/pages/popular/popular.component.ts
@@ -1,16 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-popular',
   templateUrl: './popular.component.html',
-  styles: [
-  ]
+  styles: [`
+    .clip-custom {
+      clip-path: polygon(0% 0%, 100% 0%, 100% 90%, 50% 100%, 0% 90%);
+    }
+  `]
 })
-export class PopularComponent implements OnInit {
+export class PopularComponent{
+  selectedTab = 'cartelera';
+  searchQuery = '';
 
-  constructor() { }
+  movies = [
+    ...Array(15).fill({
+      id: 0,
+      title: "Captain America: Brave New World",
+      image: "https://image.tmdb.org/t/p/w300/pzIddUEMWhWzfvLI3TwxUG2wGoi.jpg",
+      rating: 6,
+    }).map((movie, index) => ({ ...movie, id: index + 2 }))
+  ];
 
-  ngOnInit(): void {
+  movies_popular = [
+    ...Array(15).fill({
+      id: 0,
+      title: "The Vigilante",
+      image: "https://image.tmdb.org/t/p/w300/xD9LpYZYmNch2EhHWIJXXFayENH.jpg",
+      rating: 6,
+    }).map((movie, index) => ({ ...movie, id: index + 2 }))
+  ];
+
+  setTab(tab: string) {
+    this.selectedTab = tab;
   }
-
 }
