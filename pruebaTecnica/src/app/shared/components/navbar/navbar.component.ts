f2ce5616925dbd43cbb0cf3b5649dc1d8e9cecad
H4rr1ss <3601320810101@ingenieria.usac.edu.gt>
2025-03-25 23:28:40 -0600

    chore(auth): functional navbar

--- pruebaTecnica/src/app/shared/components/navbar/navbar.component.ts
+++ pruebaTecnica/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
   ]
 })
 export class NavbarComponent {
+  isLoggedIn = true;// Simulación de estado logueado
+  showMenu = false;
+  mobileMenuOpen = false;
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/auth']);
+    console.log("Redirigiendo a login...");
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+    this.showMenu = false;
+    this.mobileMenuOpen = false;
+    console.log("Sesión cerrada.");
+  }
+
+  toggleMenu() {
+    if (this.isLoggedIn) {
+      this.showMenu = !this.showMenu;
+    } else {
+      this.login();
+    }
+  }
+
+  toggleMobileMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
 }
